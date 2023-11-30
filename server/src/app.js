90fcb8a42db5bba26feba5236bd4be263c760d73
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-11-30 23:10:26 +0100

    add: get all planets route

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require('cors');
+
+const planetsRouter = require("./routes/planets.route");
 
 const app = express();
 
+// Allow CORS policy
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;

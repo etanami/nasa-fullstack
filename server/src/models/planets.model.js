236abda99598c3ad99deb9944ec37f98fd72d2b1
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-01 18:31:00 +0100

    feat: load planet data on startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,48 @@
-const model = [];
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
-module.exports = model;
+// array to store the habitable planets data from the stream
+const habitablePlanets = [];
+
+const isHabitable = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"));
+    const stream = readStream.pipe(
+      parse({
+        comment: "#",
+        columns: true,
+      })
+    );
+
+    stream.on("data", (data) => {
+      if (isHabitable(data)) {
+        habitablePlanets.push(data);
+      }
+    });
+
+    stream.on("error", (err) => {
+      console.log(err);
+      reject();
+    });
+
+    stream.on("end", () => {
+      console.log(`${habitablePlanets.length} habitable planets found!`);
+      resolve();
+    });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+}

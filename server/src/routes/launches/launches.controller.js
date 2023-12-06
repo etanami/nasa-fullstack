294ab3fae87f1348e6351027fc4bce60a212bfcc
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-06 19:56:32 +0100

    add: validation on POST request

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -7,21 +7,33 @@ function httpGetAllLaunches(req, res) {
 function httpPostLaunch(req, res) {
   const launch = req.body;
 
-  // Return error if input data is empty
-  if(!launch) {
-    res.status(404).json("Incomplete fields!")
+  // Return error if any input data is empty
+  if (
+    !launch.mission ||
+    !launch.launchDate ||
+    !launch.destination ||
+    !launch.rocket
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property!",
+    });
   }
 
-  // Format input date using date function
+  // Format input date using date function and check if it's valid
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date!",
+    });
+  }
 
   // Add new launch into model
   addNewLaunch(launch);
   // Return OK status with input data
-  res.status(201).json(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
   httpGetAllLaunches,
-  httpPostLaunch
+  httpPostLaunch,
 };

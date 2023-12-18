c7286fca1a652d38dea2c044a418343115169060
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-18 15:00:57 +0100

    connect server to database

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,27 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL =
+  "mongodb+srv://etanamiolatunji:LBBMgk7t7stvEKLd@cluster0.8exxbvs.mongodb.net/";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected!");
+});
+
+mongoose.connection.on("error", err => {
+  console.error(err)
+})
 
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {

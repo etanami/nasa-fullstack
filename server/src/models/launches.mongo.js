4d28890de86f85b60960da1af70dca25b0dbd54d
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-18 16:14:49 +0100

    feat: create new models from schemas

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  lauchDate: {
+    type: Date,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("Launch", launchesSchema);

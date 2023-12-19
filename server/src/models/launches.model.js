b354b5e7e48e253fd3089dea950bffb6e817f09f
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-12-19 16:18:53 +0100

    feat: validate target planet exists

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launches = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
 //const launches = new Map();
 
@@ -28,6 +29,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target
+  });
+
+  if(!planet) {
+    throw new Error("Planet doesn't exist");
+  }
+
   await launches.updateOne(
     {
       flightNumber: launch.flightNumber,

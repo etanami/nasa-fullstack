075c5f18bc935c266710db0b7f417b452d3e6143
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-02-02 16:28:22 +0100

    fix: mongo url connection

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://etanamiolatunji:LBBMgk7t7stvEKLd@cluster0.8exxbvs.mongodb.net/";
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connected!");
